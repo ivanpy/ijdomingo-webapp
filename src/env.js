608b72c6cd75ea920db84d4ea021b96b56c2c357
ariel.duarte <arielpy@gmail.com>
2017-04-20 20:56:48 -0300

    deploy produccion

--- src/env.js
+++ src/env.js
@@ -19,7 +19,7 @@
 
 (function (window) {
   window.__env = window.__env || {};
-  var environment = 'Dev';
+  var environment = 'Prod';
   // API url
   // For demo purposes we fetch from local file in this plunk
   // In your application this can be a url like https://api.github.com
